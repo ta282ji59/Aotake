1653a7f02f1eade8892ee7addcfd6048c31483bc
todorokitai <86834007+todorokitai@users.noreply.github.com>
2023-11-11 23:35:49 +0900

    edit

---
+++ SNS_map/SNS_map/ChatDisplay.swift
@@ -0,0 +1,29 @@
+//
+//  ChatDisplay.swift
+//  SNS_Map
+//
+//  Created by 辻 達也 on 2023/11/11.
+//
+
+import UIKit
+
+class ChatDisplay: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
