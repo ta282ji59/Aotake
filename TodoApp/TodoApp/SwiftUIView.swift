e26d2c2533588b7ce4a9a8731cf72c6a306dee0b
s1300171 <s1300171@u-aizu.ac.jp>
2023-11-13 17:09:17 +0900

    ファイルを追加

---
+++ TodoApp/TodoApp/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  TodoApp
+//
+//  Created by 新井拓希 on 2023/11/13.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        HStack {
+            Text("⬜︎")
+            Text("散歩" )
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
