612f4ff4899d613edc99aae4f2bb07118af40d8b
todorokitai <86834007+todorokitai@users.noreply.github.com>
2023-11-21 17:20:05 +0900

    add file

--- tatsuya/pre/common.js
+++ tatsuya/pre/common.js
@@ -1,16 +1,41 @@
 function checkDeviceType() {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     if (isMobile) {
-        document.getElementById('tab-bar').style.display = 'flex'; // モバイルデバイスであれば表示
+        document.getElementById('tab-bar').style.display = 'flex';
     }
 }
 
-
-
 document.addEventListener('DOMContentLoaded', checkDeviceType);
+document.addEventListener('DOMContentLoaded', loadContent('home'));
 
 
-function navigate(tab) {
-    console.log(tab + 'にナビゲートします。');
-    // ここにナビゲーションのロジックを記述
+
+/* 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+function loadContent(tab) {
+    var url;
+    if (tab == "home") {
+        url = 'home.html'
+    }
+    else if (tab == "sns") {
+        url = 'sns.html'
+    }
+    else if (tab == "account") {
+        url = 'account.html'
+    }
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'html',
+        success: function (data) {
+            $('#content').html(data);
+        },
+        error: function () {
+            alert('コンテンツのロードに失敗しました。');
+        }
+    });
+
 }
