0cbb81b29169282d1475e4b51b8827b341a3544a
todorokitai <86834007+todorokitai@users.noreply.github.com>
2023-11-29 10:21:05 +0900

    edit

--- SNS_map/SNS_map/MapDisplay.swift
+++ SNS_map/SNS_map/MapDisplay.swift
@@ -2,7 +2,7 @@ import UIKit
 import MapKit
 
 class MapDisplay: UIViewController {
-    
+
     @IBOutlet weak var mapview: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,7 +11,8 @@ class MapDisplay: UIViewController {
         let location = CLLocationCoordinate2D(latitude: 37.523671004456, longitude: 139.93913093312)
         let region = MKCoordinateRegion(center: location, latitudinalMeters: 1500, longitudinalMeters: 1500)
                 mapview.setRegion(region, animated: true)
-                
     }
 }
+
+
 
