3ffe8cfe6ed916d8954c69c1ca1d705036b4be2b
tatsuya <ta282ji@icloud.com>
2023-12-28 17:32:31 +0900

    コメント文を削除

--- Main/script/common.js
+++ Main/script/common.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
     $('#tab-bar .tab-item img').hover(
         function() {
-            // ホバー時の処理
             var src = $(this).attr('src');
-            // _clickがまだ含まれていない場合のみ変更を適用
             if (!src.includes('_click')) {
-                $(this).data('original-src', src); // 元のsrcを記録
+                $(this).data('original-src', src);
                 $(this).attr('src', src.replace('.png', '_click.png'));
             }
         }, 
         function() {
-            // ホバーが終わった時の処理
             var originalSrc = $(this).data('original-src');
-            // 元々_clickが含まれていなかった画像のみを元に戻す
             if (originalSrc) {
                 $(this).attr('src', originalSrc);
             }
